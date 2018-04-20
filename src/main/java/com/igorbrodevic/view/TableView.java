3fef240014c7b666fa17001f1ee269ee02025518
Igor Brodewicz <Igor.Brodewicz@bunge.com>
2018-04-20 17:16:08 +0200

    Table content in showing in grid

--- src/main/java/com/igorbrodevic/view/TableView.java
+++ src/main/java/com/igorbrodevic/view/TableView.java
@@ -4,6 +4,8 @@ import com.igorbrodevic.controller.CustomerService;
 import com.igorbrodevic.data.Customer;
 import com.igorbrodevic.data.Customer1;
 import com.igorbrodevic.data.HibernateUtil;
+import com.vaadin.data.provider.DataProvider;
+import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
@@ -14,17 +16,30 @@ import java.util.List;
 public class TableView extends VerticalLayout {
 
     private CustomerService service = CustomerService.getInstance();
-    private Grid grid = new Grid(Customer1.class);
+    private Grid<Customer1> grid;
+    private SingleSelect<Customer1> singleSelect;
     private TextField filterText = new TextField();
     private CustomerForm form = new CustomerForm(this);
     VerticalLayout layout = new VerticalLayout();
 
     public TableView() {
-
         CssLayout filtering = new CssLayout();
         filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
 
         Button clearFilterTextBtn  = new Button(FontAwesome.TIMES);
+
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        List<Customer1> customers1 = session.createQuery("from Customer1").list();
+
+        this.grid = new Grid<>();
+        grid.addColumn(Customer1::getCity);
+        grid.setEnabled(true);
+        grid.setItems(customers1);
+
+        session.close();
+        //grid = updateList();
+        singleSelect = grid.asSingleSelect();
 
         clearFilterTextBtn.addClickListener(e -> {
             filterText.clear();
@@ -39,11 +54,11 @@ public class TableView extends VerticalLayout {
         main.setExpandRatio(grid, 1);
 
         Button addCustomerBtn = new Button("Dodaj klienta");
-        addCustomerBtn.addClickListener(e -> {
+       /* addCustomerBtn.addClickListener(e -> {
             grid.select(null);
             form.setCustomer(new Customer());
 
-        });
+        });*/
 
         HorizontalLayout toolbar = new HorizontalLayout(filtering, addCustomerBtn);
         toolbar.setSpacing(true);
@@ -64,12 +79,12 @@ public class TableView extends VerticalLayout {
             if (event.getFirstSelectedItem().equals(null)) {
                 //form.setVisible(false);
             } else {
-                Customer customer = (Customer) event.getFirstSelectedItem().get();//event.getSelected().iterator().next();
-                form.setCustomer(customer);
+                Customer1 customer = (Customer1) event.getFirstSelectedItem().get();//event.getSelected().iterator().next();
+                //form.setCustomer(customer);
             }
         });
 
-        updateList();
+
 
         layout.setMargin(true);
         layout.setSpacing(true);
@@ -78,21 +93,27 @@ public class TableView extends VerticalLayout {
         //form.setVisible(false);
     }
 
-    public void updateList() {
-        //List<Customer> customers = service.findAll(filterText.getValue());
-        //grid.setContainerDataSource(new BeanItemContainer<>(Customer.class, customers));
-
-        grid.removeAllColumns();
+    public Grid<Customer1> updateList() {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
+        Grid<Customer1> grid = new Grid<>(Customer1.class);
+        grid.setSelectionMode(Grid.SelectionMode.SINGLE);
+
         List<Customer1> customers1 = session.createQuery("from Customer1").list();
+        //ListDataProvider<Customer1> dataProvider = DataProvider.ofCollection(customers1);
+
+
+        //grid.addColumn(customer1 -> customer1.getFirstName());
+        grid.addColumn(Customer1::getFirstName);
         grid.setItems(customers1);
-        //grid.addColumn(Customer1::getFirstName); //.setCaption("Name");
-        //grid.addColumn(Person::getBirthYear).setCaption("Year of birth");
+        //grid.setDataProvider(dataProvider);
+
+        //grid.addColumn(Customer1::getFirstName).setId("firstName");
 
 
         //grid.setContainerDataSource(new BeanItemContainer<>(Customer1.class, customers1));
         session.close();
+        return grid;
     }
 
 }
